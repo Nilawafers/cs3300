81fae044d396098145dbbcf805ea245221ff2589
Nilawafers <nbuckley@uccs.edu>
2021-12-02 17:11:06 +0000

    Added heading font style to titles

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,8 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require 'simplecov'
 SimpleCov.start 'rails' do
-  add_filter '/bin/'
-  add_filter '/db/'
+  
+  add_filter '/channels/application_cable/'
   add_filter '/spec/' # for rspec
 end
 
